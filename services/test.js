d35387baef5b400d54dd2bc0fc99e20c9db3ecee
3ddesign <3ddesign@bk.ru>
2018-01-10 11:55:43 +0200

    add frontend project

---
+++ services/test.js
@@ -0,0 +1,18 @@
+//make an ajax request using 'fetch' function
+//http://rallycoding.herokuapp.com/api/music_albums
+
+// function fetchAlbums() {
+//     fetch('http://rallycoding.herokuapp.com/api/music_albums')
+//         .then(res => res.json())
+//         .then(json => console.log(json));
+// }
+
+const fetchAlbums = async () => {
+    const res = await fetch('http://rallycoding.herokuapp.com/api/music_albums');
+    const json = await res.json();
+
+    console.log(json);
+};
+
+
+fetchAlbums();
