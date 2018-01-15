3cbd251351d23196868cfc31fa752719ac054316
3ddesign <3ddesign@bk.ru>
2018-01-15 16:55:54 +0200

    add sandgrid

--- config/dev.js
+++ config/dev.js
@@ -5,5 +5,6 @@ module.exports = {
     mongoURI: 'mongodb://root:Gh6M54Bk@ds247047.mlab.com:47047/feedbackcollection-dev',
     cookieKey: 'werweri5tpoweruiojerifjwefkpowefopw',
     stripePublishableKey: 'pk_test_RPpwcC7KEVnDuVcraMs3okSJ',
-    stripeSecretKey: 'sk_test_P6klC023Vk8HaAiD26jFbIcU'
+    stripeSecretKey: 'sk_test_P6klC023Vk8HaAiD26jFbIcU',
+    sendGridKey: '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'
 };
